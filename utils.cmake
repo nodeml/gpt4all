ef54684e88bdab7f59db964aa10a166ac4e753ad
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2023-11-19 19:34:53 -0500

    utils fix

--- utils.cmake
+++ utils.cmake
@@ -30,21 +30,23 @@ endmacro()
 function(DownloadGpt4All VERSION DESTINATION)
   if(NOT EXISTS ${DESTINATION}/gpt4all)
     
+    set(REPO_DESTINATION_DIR ${CMAKE_BINARY_DIR}/gpt4all)
+
     execute_process(
-      COMMAND git clone --depth 1 --branch v${VERSION} https://github.com/nomic-ai/gpt4all ${CMAKE_SOURCE_DIR}/repo
+      COMMAND git clone --depth 1 --branch v${VERSION} https://github.com/nomic-ai/gpt4all ${REPO_DESTINATION_DIR}
     )
 
     execute_process(
-      COMMAND git submodule update --init WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/repo
+      COMMAND git submodule update --init WORKING_DIRECTORY ${REPO_DESTINATION_DIR}
     )
     
     if(NOT EXISTS ${DESTINATION})
       file(MAKE_DIRECTORY ${DESTINATION})
     endif()
 
-    file(RENAME ${CMAKE_SOURCE_DIR}/repo/gpt4all-backend ${CMAKE_SOURCE_DIR}/deps/gpt4all)
+    file(RENAME ${REPO_DESTINATION_DIR}/gpt4all-backend ${DESTINATION}/gpt4all)
 
-    file(REMOVE ${CMAKE_SOURCE_DIR}/repo)
+    file(REMOVE ${REPO_DESTINATION_DIR})
   endif()
 endfunction()
 
